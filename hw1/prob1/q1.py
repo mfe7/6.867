3eda41cd98a6e310e2e3896177e6791023d18fca
Michael Everett <mfe@mit.edu>
2017-09-19 00:01:45 -0400

    finished p1_1 and wrote it up.

--- hw1/prob1/q1.py
+++ hw1/prob1/q1.py
@@ -32,7 +32,7 @@ def gradient_descent(f, f_params, \
         # print "diff:", diff
         num_iters += 1
 
-    return x, fx, num_iters
+    return x, fx, dx, num_iters
 
 
 def negative_gaussian(params, x):
@@ -96,28 +96,84 @@ def main():
     f_params = [gaussMean, gaussCov,multiplier]
     learning_rate = 0.01
     convergence_limit = 1e-5
-    initial_guess = np.array([8.0,18.0])
-    max_iters = 10000
-    x, fx, num_iters = gradient_descent(f,f_params,learning_rate,convergence_limit,initial_guess,max_iters)
+    max_iters = 500
 
-    # # Quadratic Bowl
-    # f = quadratic_bowl
-    # f_params = [quadBowlA, quadBowlb]
-    # learning_rate = 0.01
-    # convergence_limit = 1e-1
-    # initial_guess = np.array([2.0,-2.0])
-    # max_iters = 20
-    # x, fx, num_iters = gradient_descent(f,f_params,learning_rate,convergence_limit,initial_guess,max_iters)
+    initial_guess = np.array([8.0,8.0])
 
-    print '--------'
-    print "f([10,10]):", f(f_params,np.array([10,10]))
-    print "x:", x
-    print "fx:", fx
-    print "minimum: (%s,%s). Took %s steps." %(x[-1], fx[-1], np.shape(x)[0])
+    colors = ['b','r','g','y','c','p','b']
+    marker = []
+
+    # # Plot to show effect of initial guess.
+    # fig = plt.figure()
+    # initial_guesses = [np.array([8.0,18.0]),np.array([12.0,16.0]),np.array([8.0,8.0])]
+    # legend = []
+    # for i, initial_guess in enumerate(initial_guesses):
+    #     x, fx, dx, num_iters = gradient_descent(f,f_params,learning_rate,convergence_limit,initial_guess,max_iters)
+    #     plt.plot(range(num_iters+1), fx, '-o', c=colors[i])
+    #     legend.append('Initial Guess Error Norm: '+str(round(np.linalg.norm(initial_guess - x[-1]),2)))
+    # plt.legend(legend)
+    # plt.xlabel("Iteration Number")
+    # plt.ylabel("f(x) [Negative Gaussian]")
+    # plt.show()
+
+    # # Plot to show effect of step size on convergence
+    # fig = plt.figure()
+    # convergence_limits = [1e-4,1e-3,1e-2,1e-1]
+    # legend = []
+    # for i, convergence_limit in enumerate(convergence_limits):
+    #     x, fx, dx, num_iters = gradient_descent(f,f_params,learning_rate,convergence_limit,initial_guess,max_iters)
+    #     plt.plot(range(num_iters+1), fx, '-o', c=colors[i])
+    #     legend.append('Convergence Limit: '+str(convergence_limit))
+    # plt.legend(legend)
+    # plt.xlabel("Iteration Number")
+    # plt.ylabel("f(x) [Negative Gaussian]")
+    # plt.show()
+
+    # fig = plt.figure()
+    # convergence_limits = np.logspace(2,-3,20)
+    # print convergence_limits
+    # legend = []
+    # solutions = []
+    # for i, convergence_limit in enumerate(convergence_limits):
+    #     x, fx, dx, num_iters = gradient_descent(f,f_params,learning_rate,convergence_limit,initial_guess,max_iters)
+    #     solutions.append(fx[-1])
+    # plt.semilogx(convergence_limits, solutions,'o')
+    # plt.xlabel("Convergence Limit")
+    # plt.ylabel("f(x) [Negative Gaussian]")
+    # plt.show()
+
+    # Quadratic Bowl
+    f = quadratic_bowl
+    f_params = [quadBowlA, quadBowlb]
+    learning_rate = 0.01
+    convergence_limit = 1e-1
+    initial_guess = np.array([2.0,-2.0])
+    max_iters = 20
+    x, fx, dx, num_iters = gradient_descent(f,f_params,learning_rate,convergence_limit,initial_guess,max_iters)
+
+    # print '--------'
+    # print "f([10,10]):", f(f_params,np.array([10,10]))
+    # print "x:", x
+    # print "fx:", fx
+    # print "dx:", dx
+    # print "minimum: (%s,%s). Took %s steps." %(x[-1], fx[-1], np.shape(x)[0])
+
+    # fig = plt.figure()
+    # plt.scatter(range(num_iters+1), fx)
+    # plt.xlabel("Iteration Number")
+    # plt.ylabel("f(x)")
+    # plt.show()
+
+
+    fig2 = plt.figure()
+    plt.plot(range(num_iters+1), np.linalg.norm(dx, axis=1),'-o')
+    plt.xlabel("Iteration Number")
+    plt.ylabel("Norm of Gradient [Quadratic Bowl]")
+    plt.show()
 
     # fig = plt.figure()
     # ax = fig.add_subplot(111, projection='3d')
-    
+
     # Z = negative_gaussian(f_params,)
     # Axes3D.plot_surface(X, Y, Z, *args, **kwargs)
 
--- hw1/prob1/q3.py
+++ hw1/prob1/q3.py
@@ -4,7 +4,7 @@ import loadFittingDataP1 as load_data
 import q1, q2
 
 def sgd(f, f_params, \
-    learning_rate, convergence_limit, initial_guess, max_iters):
+    convergence_limit, initial_guess, max_iters, t0, k):
     # 
     # Approximate the minimum of a function minimum via stochastic gradient descent,
     # after reaching max_iters or significantly small difference
@@ -13,28 +13,28 @@ def sgd(f, f_params, \
     # Return x (np array of x values throughout descent)
     #         and fx (np array of f(x) values throughout descent)
 
-    # SGD params (learning rate schedule)
-    t0 = 1e10
-    k = 0.6
-
     diff = np.inf
-    num_iters = 0
+    num_iters = 1
     x = np.array([initial_guess])
     initial_fx, initial_dx = f(f_params,initial_guess)
     fx = np.array([initial_fx])
     dx = np.array([initial_dx])
-    while diff > convergence_limit and num_iters < max_iters:
+    while diff > convergence_limit and num_iters < max_iters+1:
         learning_rate = (t0 + num_iters)**(-k)
         next_x = x[-1] - learning_rate * dx[-1]
+        print '------'
+        print x[-1]
+        print learning_rate
+        print dx[-1]
+
         next_fx, next_dx = f(f_params, next_x)
         x = np.vstack([x, next_x])
         fx = np.vstack([fx, next_fx])
         dx = np.vstack([dx, next_dx])
         diff = np.linalg.norm(fx[-1] - fx[-2])
-        # print "diff:", diff
         num_iters += 1
 
-    return x, fx, num_iters
+    return x, fx, num_iters - 1
 
 def lsq_gradient(Theta, xi, yi):
     #
@@ -67,6 +67,9 @@ def least_square_error(f_params, Theta):
     random_y = y[i]
     g = lsq_gradient(Theta, random_x, random_y)
 
+    # print X
+    # print X[i]
+    # print y[i]
     return J, g
 
 def lsq(f_params, x):
@@ -88,9 +91,18 @@ def main():
     # Theta_pinv = np.dot(np.linalg.pinv(X),y)
     # J_Theta_pinv, _ = lsq(f_params, Theta_pinv)
     # print "-------------"
-    # print "Analytic:"
+    # print "Analytic (Pseudoinverse):"
     # print "Theta: %s" %(Theta_pinv)
     # print "J(Theta): %s" %(J_Theta_pinv)
+    # print "-------------"
+
+    # # Analytic solution
+    # Theta_exact = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(X),X)),np.transpose(X)),y)
+    # J_Theta_exact, _ = lsq(f_params, Theta_exact)
+    # print "-------------"
+    # print "Analytic inv(X'X)X'Y:"
+    # print "Theta: %s" %(Theta_exact)
+    # print "J(Theta): %s" %(J_Theta_exact)
     # print "-------------"
 
     # # Batch GD params
@@ -109,12 +121,15 @@ def main():
 
     # SGD params
     f = least_square_error
-    learning_rate = 0.00001
-    convergence_limit = 1e-5
+    convergence_limit = 1e0
+    # initial_guess = np.array([0.503, -2.33, -6.31, 6.81,  -1.06, \
+        # 6.67, 3.41, -0.45, -12.94, 15.73])
     initial_guess = np.zeros([np.shape(X)[1]])
-    max_iters = 10000
+    max_iters = 20
+    t0 = 1e8 # learning rate
+    k = 0.6 # learning rate
 
-    Theta_SGD, J_Theta_SGD, num_iters_SGD = sgd(f,f_params,learning_rate,convergence_limit,initial_guess,max_iters)
+    Theta_SGD, J_Theta_SGD, num_iters_SGD = sgd(f,f_params,convergence_limit,initial_guess,max_iters, t0, k)
     print "-------------"
     print "Stochastic GD:"
     print "Theta: %s" %(Theta_SGD[-1])
@@ -123,6 +138,7 @@ def main():
     print "-------------"
     
     return
+
 
 if __name__ == '__main__':
     main()
