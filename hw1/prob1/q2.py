d8f294184d901a9a25c7881dd7d0265b83166e1f
Michael Everett <mfe@mit.edu>
2017-09-19 23:54:03 -0400

    Wrote up 1.2 as well

--- hw1/prob1/q2.py
+++ hw1/prob1/q2.py
@@ -1,6 +1,8 @@
 import numpy as np
 import loadParametersP1 as load_params
 import q1
+import matplotlib.pyplot as plt
+
 
 def gradient_approximation(f, f_params, x, dx):
     #
@@ -31,10 +33,6 @@ def main():
     f = q1.negative_gaussian
     multiplier = 1e4
     f_params = [gaussMean, gaussCov,multiplier]
-    
-    # # Quadratic Bowl
-    # f = q1.quadratic_bowl
-    # f_params = [quadBowlA, quadBowlb]
 
     # Gradient params
     x = np.array([3.0,4.0])
@@ -45,7 +43,69 @@ def main():
     approx_gradient = gradient_approximation(f, f_params, x, dx)
     gradient_error = approx_gradient - real_gradient
     error_norm = np.linalg.norm(gradient_error)
+    print x
     print "True: %s, Approx: %s, Error: %s, Error Norm: %s" %(real_gradient, approx_gradient, gradient_error, error_norm)
+    print "------"
+
+    x = np.array([3.0,4.0])
+    error_norms = []
+    dxs = np.logspace(0,-5)
+    for dx in dxs:
+        real_gradient = f(f_params, x)[1]
+        approx_gradient = gradient_approximation(f, f_params, x, dx)
+        gradient_error = approx_gradient - real_gradient
+        error_norm = np.linalg.norm(gradient_error)
+        error_norms.append(error_norm)
+    fig = plt.figure()
+    plt.semilogx(dxs, error_norms, '-o')
+    plt.xlabel('$\delta x$: Difference Step')
+    plt.ylabel('Norm of Error between Real and Approximate Gradient')
+    plt.show()
+
+
+    # Gradient params
+    x = np.array([12.0,4.0])
+    dx = 0.01
+
+    # Calculate real and approx gradients
+    real_gradient = f(f_params, x)[1]
+    approx_gradient = gradient_approximation(f, f_params, x, dx)
+    gradient_error = approx_gradient - real_gradient
+    error_norm = np.linalg.norm(gradient_error)
+    print x
+    print "True: %s, Approx: %s, Error: %s, Error Norm: %s" %(real_gradient, approx_gradient, gradient_error, error_norm)
+    print "------"
+
+
+    # Quadratic Bowl
+    f = q1.quadratic_bowl
+    f_params = [quadBowlA, quadBowlb]
+
+    # Gradient params
+    x = np.array([6.0,2.0])
+    dx = 0.01
+
+    # Calculate real and approx gradients
+    real_gradient = f(f_params, x)[1]
+    approx_gradient = gradient_approximation(f, f_params, x, dx)
+    gradient_error = approx_gradient - real_gradient
+    error_norm = np.linalg.norm(gradient_error)
+    print x
+    print "True: %s, Approx: %s, Error: %s, Error Norm: %s" %(real_gradient, approx_gradient, gradient_error, error_norm)
+    print "------"
+
+    # Gradient params
+    x = np.array([1000.0,90.0])
+    dx = 0.01
+
+    # Calculate real and approx gradients
+    real_gradient = f(f_params, x)[1]
+    approx_gradient = gradient_approximation(f, f_params, x, dx)
+    gradient_error = approx_gradient - real_gradient
+    error_norm = np.linalg.norm(gradient_error)
+    print x
+    print "True: %s, Approx: %s, Error: %s, Error Norm: %s" %(real_gradient, approx_gradient, gradient_error, error_norm)
+    print "------"
 
 
 if __name__ == '__main__':
