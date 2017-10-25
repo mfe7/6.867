5aff13ae8e1f4095ba23e89b55bd73461fd90250
Michael Everett <mfe@mit.edu>
2017-10-24 21:08:39 -0400

    done w hw2

--- hw2/code/mnist.py
+++ hw2/code/mnist.py
@@ -3,6 +3,9 @@ import pylab as pl
 from sklearn import linear_model, svm
 import matplotlib.pyplot as plt
 
+import pegasos_gaussian_test as peg
+import svm_test as qp
+
 trainSize = 200
 valSize = 150
 testSize = 150
@@ -73,6 +76,11 @@ def accuracy(x, y, clf):
     acc = (100.0*(len(x)-len(incorrect_inds)))/len(x)
     print acc
 
+def accuracy2(x, y, preds):
+    incorrect_inds = np.where(preds != y)[0]
+    acc = (100.0*(len(x)-len(incorrect_inds)))/len(x)
+    print acc
+
 classes = [[[1],[7]],[[3],[5]],[[4],[9]],[[0,2,4,6,8],[1,3,5,7,9]]]
 
 lr_Cs = [1e-3, 1e-1, 1e0, 1e2]
@@ -83,14 +91,15 @@ lsvm_Cs = [1e-6, 1e-5, 1e-5, 1e-1]
 
 n_svc_gammas = [0.01, 0.001, 0.001, 0.01]
 n_svc_Cs = [1, 100, 10, 1]
-# n_svc_Cs = [1e-5, 1e-5, 1e-5, 1e-5]
+svc_gammas = [1e-6, 1e-5, 0.001, 0.01]
+svc_Cs = [1e-10, 1e-10, 1e-5, 1e-5]
 
-# for i, c in enumerate([[[1],[7]]]):
-for i, c in enumerate(classes):
+for i, c in enumerate([[[4],[9]]]):
+# for i, c in enumerate(classes):
     print "Class 1:", c[0], "vs.", c[1]
     class1 = np.array(c[0])
     class2 = np.array(c[1])
-    x_train, x_val, x_test, y_train, y_val, y_test = createX(class1, class2,normalize_data=True)
+    x_train, x_val, x_test, y_train, y_val, y_test = createX(class1, class2,normalize_data=False)
 
     # for penalty in ['l1','l2']:
     #     print "Penalty:", penalty
@@ -146,9 +155,9 @@ for i, c in enumerate(classes):
     # print "Testing acc:"
     # accuracy(x_test, y_test, clf_linear_svc)
 
-    # for gamma in np.logspace(-5,5,num=11):
+    # for gamma in np.logspace(-10,-5, num=6):
     #     print "gamma:", gamma
-    #     for C in np.logspace(-5,5,num=11):
+    #     for C in np.logspace(-10,-5,num=6):
     #         clf_rbf_svc = svm.SVC(C=C, cache_size=200, class_weight=None, coef0=0.0,
     #            decision_function_shape='ovr', degree=3, gamma=gamma, kernel='rbf',
     #            max_iter=-1, probability=False, random_state=None, shrinking=True,
@@ -156,17 +165,52 @@ for i, c in enumerate(classes):
     #         clf_rbf_svc.fit(x_train, y_train)
     #         print "C:", C
     #         accuracy(x_val, y_val, clf_rbf_svc) 
+
     
-    # Linear SVM
-    # C = lsvm_Cs[i]
-    C = n_svc_Cs[i]
-    gamma = n_svc_gammas[i]
-    clf_rbf_svc = svm.SVC(C=C, cache_size=200, class_weight=None, coef0=0.0,
-       decision_function_shape='ovr', degree=3, gamma=gamma, kernel='rbf',
-       max_iter=-1, probability=False, random_state=None, shrinking=True,
-       tol=0.001, verbose=False)
-    clf_rbf_svc.fit(x_train, y_train)
-    accuracy(x_train, y_train, clf_rbf_svc)
-    accuracy(x_test, y_test, clf_rbf_svc)
+    # # Linear SVM
+    # # C = n_svc_Cs[i]
+    # # gamma = n_svc_gammas[i]
+    # C = svc_Cs[i]
+    # gamma = svc_gammas[i]
+    # clf_rbf_svc = svm.SVC(C=C, cache_size=200, class_weight=None, coef0=0.0,
+    #    decision_function_shape='ovr', degree=3, gamma=gamma, kernel='rbf',
+    #    max_iter=-1, probability=False, random_state=None, shrinking=True,
+    #    tol=0.001, verbose=False)
+    # clf_rbf_svc.fit(x_train, y_train)
+    # accuracy(x_train, y_train, clf_rbf_svc)
+    # accuracy(x_test, y_test, clf_rbf_svc)
+
+
+    # # Pegasos
+    # lmbda = .02;
+    # epochs = 100;
+    # for gamma in np.logspace(-10,-5, num=6):
+    #     print "gamma:", gamma
+    #     K = peg.gaussian_rbf_matrix(x_train, gamma)
+    #     alpha = peg.train_gaussianSVM(x_train,y_train, K, lmbda, epochs)
+    #     preds_val = peg.predict_gaussianSVM(x_val, alpha, x_train, gamma)
+    #     accuracy2(x_val, y_val, preds_val)
+
+    # QP
+    epochs = 100;
+    gaussianRBF_lambda = 0.2
+    for C in np.logspace(-10,-5, num=6):
+        kernel = qp.gaussianRBFKernel
+        w, bias, alpha, nonzero_alpha_inds = qp.trainSVM(x_train,y_train,kernel=kernel,C=C)
+        preds_val = peg.predictSVM(x_val,x_train,y_train,bias,alpha,kernel=kernel)
+        accuracy2(x_val, y_val, preds_val) 
+
+
+    # # C = n_svc_Cs[i]
+    # # gamma = n_svc_gammas[i]
+    # C = svc_Cs[i]
+    # gamma = svc_gammas[i]
+    # clf_rbf_svc = svm.SVC(C=C, cache_size=200, class_weight=None, coef0=0.0,
+    #    decision_function_shape='ovr', degree=3, gamma=gamma, kernel='rbf',
+    #    max_iter=-1, probability=False, random_state=None, shrinking=True,
+    #    tol=0.001, verbose=False)
+    # clf_rbf_svc.fit(x_train, y_train)
+    # accuracy(x_train, y_train, clf_rbf_svc)
+    # accuracy(x_test, y_test, clf_rbf_svc)
      
 
--- hw2/code/pegasos_gaussian_test.py
+++ hw2/code/pegasos_gaussian_test.py
@@ -48,57 +48,63 @@ def predict_gaussianSVM(x, alpha, x_train, gamma):
     return np.dot(alpha, gaussian_rbf(x_train, x, gamma))
 
 
+def main():
+
+    #############
+    # Decision boundaries for 4 datasets
+    ###############
+
+    datasets = [3]
+    # datasets = [1,2,3,4]
+    colors = ['g','b','y','r','m','c']
+    C = 1.0
+
+    # gammas = [2e2, 2e1, 2e0, 2e-1, 2e-2]
+    gammas = [2e2, 2e1, 2e0, 2e-1]
+
+    dataset = 3
+    train = loadtxt('data/data'+str(dataset)+'_train.csv')
+    X_train = train[:,0:2]
+    Y_train = train[:,2:3]
+    validate = loadtxt('data/data'+str(dataset)+'_validate.csv')
+    X_val = validate[:,0:2]
+    Y_val = validate[:,2:3]
+    # test = loadtxt('data/data'+str(dataset)+'_test.csv')
+    # X_test = test[:,0:2]
+    # Y_test = np.squeeze(test[:,2:3])
+    num_training_pts = float(np.shape(Y_train)[0])
+    num_val_pts = float(np.shape(Y_val)[0])
+
+    pl.figure()
+    for i, gamma in enumerate(gammas):
+        pl.subplot(1,4,i+1)
+        x_min, x_max = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
+        y_min, y_max = X_train[:, 1].min() - 1, X_train[:, 1].max() + 1
+        # h = max((x_max-x_min)/50., (y_max-y_min)/50.)
+        h = max((x_max-x_min)/200., (y_max-y_min)/200.)
+        xx, yy = meshgrid(arange(x_min, x_max, h),
+                          arange(y_min, y_max, h))
+
+        pl.scatter(X_train[:, 0], X_train[:, 1], c=(1.-Y_train), s=50, cmap = pl.cm.cool)
+
+        K = gaussian_rbf_matrix(X_train, gamma)
+        alpha = train_gaussianSVM(X_train,Y_train, K, lmbda, epochs)
+        nonzero_alpha_inds = np.where(alpha > 1e-4)[0]
+        print "gamma:", gamma, "num non-zero alphas:", len(nonzero_alpha_inds)
+        # zz = array([predict_gaussianSVM(x, alpha, X_train, gamma) for x in c_[xx.ravel(), yy.ravel()]])
+        # print zz
+        # zz = zz.reshape(xx.shape)
+        # CS = pl.contour(xx, yy, zz, [-1, 0, 1], linestyles = 'solid', linewidths = 2)
+        # pl.clabel(CS, fontsize=9, inline=1)
+        # pl.axis('tight')
+
+    # pl.savefig("../paper/figures/3_3_decisions")
 
 
-#############
-# Decision boundaries for 4 datasets
-###############
-
-datasets = [3]
-# datasets = [1,2,3,4]
-colors = ['g','b','y','r','m','c']
-C = 1.0
-
-# gammas = [2e2, 2e1, 2e0, 2e-1, 2e-2]
-gammas = [2e2, 2e1, 2e0, 2e-1]
-
-dataset = 3
-train = loadtxt('data/data'+str(dataset)+'_train.csv')
-X_train = train[:,0:2]
-Y_train = train[:,2:3]
-validate = loadtxt('data/data'+str(dataset)+'_validate.csv')
-X_val = validate[:,0:2]
-Y_val = validate[:,2:3]
-# test = loadtxt('data/data'+str(dataset)+'_test.csv')
-# X_test = test[:,0:2]
-# Y_test = np.squeeze(test[:,2:3])
-num_training_pts = float(np.shape(Y_train)[0])
-num_val_pts = float(np.shape(Y_val)[0])
-
-pl.figure()
-for i, gamma in enumerate(gammas):
-    pl.subplot(1,4,i+1)
-    x_min, x_max = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
-    y_min, y_max = X_train[:, 1].min() - 1, X_train[:, 1].max() + 1
-    # h = max((x_max-x_min)/50., (y_max-y_min)/50.)
-    h = max((x_max-x_min)/200., (y_max-y_min)/200.)
-    xx, yy = meshgrid(arange(x_min, x_max, h),
-                      arange(y_min, y_max, h))
-
-    pl.scatter(X_train[:, 0], X_train[:, 1], c=(1.-Y_train), s=50, cmap = pl.cm.cool)
-
-    K = gaussian_rbf_matrix(X_train, gamma)
-    alpha = train_gaussianSVM(X_train,Y_train, K, lmbda, epochs)
-    nonzero_alpha_inds = np.where(alpha > 1e-4)[0]
-    print "gamma:", gamma, "num non-zero alphas:", len(nonzero_alpha_inds)
-    # zz = array([predict_gaussianSVM(x, alpha, X_train, gamma) for x in c_[xx.ravel(), yy.ravel()]])
-    # print zz
-    # zz = zz.reshape(xx.shape)
-    # CS = pl.contour(xx, yy, zz, [-1, 0, 1], linestyles = 'solid', linewidths = 2)
-    # pl.clabel(CS, fontsize=9, inline=1)
-    # pl.axis('tight')
-
-# pl.savefig("../paper/figures/3_3_decisions")
+    pl.show()
 
 
-pl.show()
+if __name__ == '__main__':
+    main()
+
+
