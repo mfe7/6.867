b7740f8720b604569a5a2532fd8cdb0dd032d38a
Michael Everett <mfe@mit.edu>
2017-10-29 15:21:58 -0400

    updates. local frame extraction seems ok now

--- final_project/trajectory_data/process_tables.m
+++ final_project/trajectory_data/process_tables.m
@@ -72,12 +72,20 @@ else
     clusters = struct('id',{},'time',{},'x',{},'y',{},'easting',{},'northing',{},'color',{});
     clusters2 = struct('id',{},'time',{},'x',{},'y',{},'easting',{},'northing',{},'color',{},'local_x',{},'local_y',{});
     if ~isempty(table_p)
+        
+        num_crosses = 0;
         unique_vehicle_ids = unique(table_p.vehicle_id);
         for v=1:length(unique_vehicle_ids) % go through each vehicle
             vehicle_table_p = table_p(strcmp(table_p.vehicle_id,unique_vehicle_ids(v)),:);
             vehicle_table_v = table_v(strcmp(table_v.vehicle_id,unique_vehicle_ids(v)),:);
             cluster_ids = unique(vehicle_table_p.ped_id);
             for i=1:length(cluster_ids) % go through each cluster
+                
+                % Extract pedestrian cluster time vector, and vehicle's
+                % time vector.
+                % Find index of vehicle's time vector that's closest to
+                % each pedestrian timestamp to make sure positions
+                % are synchronized.
                 t_ped = vehicle_table_p.time(vehicle_table_p.ped_id == cluster_ids(i));
                 t_veh = vehicle_table_v.time;
                 t_align = zeros(length(t_veh), length(t_ped));
@@ -93,25 +101,45 @@ else
                 while ~offset_complete
                     veh_p2 = [vehicle_table_v.x(ind_align_offset), vehicle_table_v.y(ind_align_offset)];
                     veh_delta = veh_p2 - veh_p1;
-                    zero_inds = find(veh_delta == [0, 0])
+                    zero_inds = find(~any(veh_delta,2));
                     if length(zero_inds) > 0
-                        ind_align_offset(zero_inds)
-                        display('something')
-%                         ind_align_offset(zero_inds) = ind_align_offset(zero_inds) + 1;
+                        ind_align_offset(zero_inds) = ind_align_offset(zero_inds) + 1;
                     else
                         offset_complete = 1;
                     end
                 end
-%                 veh_delta = veh_p2 - veh_p1;
-%                 veh_p2 = [vehicle_table_v.x(ind_align), vehicle_table_v.y(ind_align)];
-%                 
-%                 
-%                 [dt index] = min(abs(t_ped - vehicle_table_v.time));
-%                 veh_x = vehicle_table_v.x(index);
-%                 veh_y = vehicle_table_v.y(index);
-%                 ped_x = vehicle_table_p.x(vehicle_table_p.ped_id == cluster_ids(i));
-%                 ped_y = vehicle_table_p.y(vehicle_table_p.ped_id == cluster_ids(i));
+                veh_delta = veh_p2 - veh_p1;
+                r_parallel = normr(veh_delta);
+                r_orthog = [-r_parallel(:,2), r_parallel(:,1)];
                 
+                ped_x = vehicle_table_p.x(vehicle_table_p.ped_id == cluster_ids(i));
+                ped_y = vehicle_table_p.y(vehicle_table_p.ped_id == cluster_ids(i));
+                d = [ped_x ped_y] - veh_p1;
+                ped_parallel = dot(d, r_parallel,2);
+                ped_orthog = dot(d, r_orthog,2);
+                % Pedestrian position in local vehicle frame
+                ped_local = [ped_parallel, ped_orthog];
+                
+                % Check if ped_local is ever within rectangle in front
+                % of vehicle
+                top_left = veh_p1 + [10, -2];
+                top_right = veh_p1 + [10, 2];
+                bottom_left = veh_p1 + [0, -2];
+                bottom_right = veh_p1 + [0, 2];
+                veh_polygon = [top_left;top_right;bottom_right;bottom_left];
+                
+                ped_crosses_in_front = 0;
+                for t=1:length(t_ped)
+                    if inpolygon(ped_local(t,1), ped_local(t,2), veh_polygon(:,1), veh_polygon(:,2))
+                        ped_crosses_in_front = 1;
+                    end
+                end
+                if ped_crosses_in_front
+                    num_crosses = num_crosses + 1;
+                    display('cross!')
+                else
+                    display('no cross.')
+                end
             
                 
                 clusters(end+1).id = length(clusters)+1;
@@ -125,6 +153,9 @@ else
                 clusters(end).color = color;
             end
         end
+        display(num_crosses)
+        display(length(cluster_ids))
+        display(num_crosses/length(cluster_ids))
         display("done")
 
         %% Remove short/long clusters
